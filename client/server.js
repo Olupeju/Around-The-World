3d4f83df2bebe04d9b0b6b3fbf1e2b2d5dfecef5
unknown <olayika@hotmail.com>
2019-03-21 23:05:28 -0500

    install json package

---
+++ client/server.js
@@ -0,0 +1,30 @@
+const express = require ("express");
+const path = require ("path");
+const mongoose = require("mongoose");
+const logger = require ("morgan");
+
+const app = express ();
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Around-The-World";
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(logger("dev"));
+
+mongoose.connect(MONGODB_URI);
+
+const db = require(path.join(__dirname, "./models/"));
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+require('./routes/dataRoutes')(app, db);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
+})
+
+
