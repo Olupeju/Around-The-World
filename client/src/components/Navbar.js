a3cc5493c1492f878888b431586dc222522f262f
unknown <olayika@hotmail.com>
2019-04-03 10:01:21 -0500

    modified navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,74 @@
 import React from "react";
-// import {Link} from "react-router-dom";
-import {Container} from "reactstrap";
+import {Link} from "react-router-dom";
+// import {Container} from "reactstrap";
 import "./Navbar.css";
 
 class Navbar extends React.Component{
     render(){
         return(
-        <Container>
-            <Navbar expand="lg" variant="light" bg="light">
-                <Navbar.Brand href="#">Around The World</Navbar.Brand>
-            </Navbar>
-        </Container>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button 
+                className = "navbar-toggler" 
+                type = "button" 
+                data-toggle = "collapse" 
+                data-target = "#navbarNav" 
+                aria-controls = "navbarNav" 
+                aria-expanded = "false" 
+                aria-label = "Toggle navigation"
+            >
+                <span className = "navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="nav navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/Airline">
+                            <div className = "nav-link">   
+                                Airline
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/FlightTracker">
+                            <div className = "nav-link">   
+                                 FlightTracker
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Culture">
+                            <div className = "nav-link">   
+                                Culture
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/food">
+                            <div className = "nav-link">   
+                                Food Tracker
+                            </div>
+                        </Link>
+                    </li>
+                    {/* <li className="nav-item">
+                        <Link to= "/bmi">
+                            <div className="nav-link">
+                                BMI
+                            </div>
+                        </Link>
+                    </li> */}
+                </ul>
+            </div>
+        </nav>
+        // <Container>
+        //     <Navbar expand="lg" variant="light" bg="light">
+        //         <Navbar.Brand href="#">Around The World</Navbar.Brand>
+        //     </Navbar>
+        // </Container>
         )
         
     }
    
-}
+};
 
 
 export default Navbar;
